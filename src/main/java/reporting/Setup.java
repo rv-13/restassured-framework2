74b5395f48952cac0114554deec5e384051b3be4
rv-13 <r131194@gmail.com>
2023-08-07 16:47:00 +0530

    FrameWork development

---
+++ src/main/java/reporting/Setup.java
@@ -0,0 +1,28 @@
+package reporting;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Setup implements ITestListener {
+    private static ExtentReports extentReports;
+    public static ThreadLocal<ExtentTest> extentTestThreadLocal = new ThreadLocal<>();
+
+    public void onStart(ITestContext context) {
+        String fileName = ExtentReportingManager.getReportNameWithTimeStamp();
+        String fullReportPath = System.getProperty("user.dir") + "\\reports\\" + fileName;
+
+        extentReports = ExtentReportingManager.createInstance(fileName, "Test API Automation Report", "Test Execution Report");
+    }
+
+    public void onFinish(ITestContext context) {
+        if (extentReports != null) extentReports.flush();
+    }
+
+    public void onTestStart(ITestResult result) {
+        ExtentTest test = extentReports.createTest("Test Name:-" + result.getTestClass().getName() + "-" + result.getMethod().getMethodName());
+        extentTestThreadLocal.set(test);
+    }
+}
