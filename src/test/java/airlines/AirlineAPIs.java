1a118584e5ea7346f86e25826ed101ee7dbf9be2
rv-13 <r131194@gmail.com>
2023-08-20 16:08:50 +0530

    FrameWork development

--- src/test/java/airlines/AirlineAPIs.java
+++ src/test/java/airlines/AirlineAPIs.java
@@ -1,5 +1,6 @@
 package airlines;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.restassured.response.Response;
 import restUtils.RestUtils;
 
@@ -7,14 +8,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AirlineAPIs {
-    public Response createAirline(Map<String, Object> createAirlinePayload) {
+    public Response createAirline(Map<String, Object> createAirlinePayload) throws JsonProcessingException {
 
         String endPoint = (String) Base.dataFromJsonFile.get("createAirLineEndpoint");
         Response response = RestUtils.performPostFromMap(endPoint, createAirlinePayload, new HashMap<>());
         return response;
     }
 
-    public Response createAirlineFromStringPayload(String createAirlinePayload) {
+    public Response createAirlineFromStringPayload(String createAirlinePayload) throws JsonProcessingException {
 
         String endPoint = (String) Base.dataFromJsonFile.get("createAirLineEndpoint");
         Response response = RestUtils.performPost(endPoint, createAirlinePayload, new HashMap<>());
