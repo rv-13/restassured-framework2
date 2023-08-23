304c1371551b76eae8edfd05fb647f2a7ab12afb
rv-13 <r131194@gmail.com>
2023-08-23 15:29:28 +0530

    FrameWork development

--- src/test/java/user/UserTests.java
+++ src/test/java/user/UserTests.java
@@ -1,12 +1,16 @@
 package user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pojos.User;
+import restutilities.AssertionUtils;
+import utils.ExcelUtils;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.*;
 
 public class UserTests extends UserAPIs {
 
@@ -22,6 +26,46 @@ public class UserTests extends UserAPIs {
     public void createUserFromPojo() throws IOException {
         User createUserPayload = new User();
         Response response = createUserFromPojo(createUserPayload);
+        Assert.assertEquals(response.statusCode(), 201);
+
+    }
+
+    @Test
+    public void createUserAndVerifyResponse() throws IOException {
+        User createUserPayload = new User();
+        Response response = createUserFromPojo(createUserPayload);
+        ObjectMapper objectMapper = new ObjectMapper();
+        User createUserResponse = objectMapper.readValue(response.getBody().asString(), User.class);
+        Assert.assertEquals(createUserResponse, createUserPayload);
+    }
+
+    @Test
+    public void createUserAndVerifyAssertions() throws IOException {
+        User createUserPayload = new User();
+        Response response = createUserFromPojo(createUserPayload);
+        Map<String, Object> expectedValueMap = new HashMap<>();
+        expectedValueMap.put("name", createUserPayload.getName());
+        expectedValueMap.put("job", createUserPayload.getJob());
+        AssertionUtils.assertExpectedValuesWithJsonPath(response, expectedValueMap);
+    }
+
+    @DataProvider(name = "userDataProvider")
+    public Iterator<User> getUsersDataFromExcel() throws IOException {
+        List<LinkedHashMap<String, String>> excelDataAsListOfMap = ExcelUtils.getExcelDataAsListOfMap("TestData_restassured2", "userTestsData");
+        List<User> userData = new ArrayList<>();
+        for (LinkedHashMap<String, String> dataMap : excelDataAsListOfMap) {
+            User user = User.builder()
+                    .name(dataMap.get("name"))
+                    .job(dataMap.get("job")).build();
+            userData.add(user);
+
+        }
+        return userData.iterator();
+    }
+
+    @Test(dataProvider = "userDataProvider")
+    public void createUserFromPojoThroughDataProvider(User user) throws IOException {
+        Response response = createUserFromPojo(user);
         Assert.assertEquals(response.statusCode(), 201);
 
     }
